3bcdff92c80c58ee60854f195bd813558363d6b9
Yryskeldi <ymajrambekov@gmail.com>
2024-07-19 20:49:44 +0600

    deleted interceptor and okHttpClient

--- app/src/main/java/com/example/aidar_badirov_hw_5_3/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/aidar_badirov_hw_5_3/network/RetrofitInstance.kt
@@ -9,15 +9,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInstance {
     companion object {
         private val retrofitInstance by lazy {
-            val logging = HttpLoggingInterceptor()
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build()
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
                 .build()
         }
 
--- app/src/main/java/com/example/aidar_badirov_hw_5_3/ui/fragments/ResultViewModel.kt
+++ app/src/main/java/com/example/aidar_badirov_hw_5_3/ui/fragments/ResultViewModel.kt
@@ -3,17 +3,29 @@ package com.example.aidar_badirov_hw_5_3.ui.fragments
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.aidar_badirov_hw_5_3.model.ResultResponse
-import com.example.aidar_badirov_hw_5_3.repository.ResultRepository
+import com.example.aidar_badirov_hw_5_3.network.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ResultViewModel : ViewModel() {
 
-    private val resultRepository = ResultRepository()
-
     val resultLiveData = MutableLiveData<ResultResponse>()
+    val errorData = MutableLiveData<String>()
 
     fun getResult(yourName: String, partnerName: String) {
-        resultRepository.getResult(yourName, partnerName).observeForever { result ->
-            resultLiveData.postValue(result)
-        }
+        RetrofitInstance.api.getResult(yourName, partnerName).enqueue(object : Callback<ResultResponse> {
+            override fun onResponse(call: Call<ResultResponse>, response: Response<ResultResponse>) {
+                if (response.body() != null && response.isSuccessful) {
+                    resultLiveData.postValue(response.body())
+                } else {
+                    errorData.postValue(response.message())
+                }
+            }
+
+            override fun onFailure(call: Call<ResultResponse>, t: Throwable) {
+                errorData.postValue(t.localizedMessage)
+            }
+        })
     }
 }
